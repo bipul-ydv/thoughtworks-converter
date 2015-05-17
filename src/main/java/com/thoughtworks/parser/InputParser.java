4fd64e040c8e3035f12fcfca261721655c0ceac2
Daniel Lanciana <daniel.lanciana@gmail.com>
2015-05-17 12:27:07 -0400

    test(InputParser): regular expression matching, all tests pass

--- src/main/java/com/thoughtworks/parser/InputParser.java
+++ src/main/java/com/thoughtworks/parser/InputParser.java
@@ -10,6 +10,10 @@ import com.thoughtworks.writer.Writer;
 
 public class InputParser {
     private static final String WRITER_LINE_PREFIX = "how ";
+    private static final String REGEX_SET_SYMBOL = "^[a-z]+( is )(i|v|x|l|c|d|m)$";
+    private static final String REGEX_SET_CREDIT = "^[a-z ]*[a-z]+( is )[0-9]+( credits)$";
+    private static final String REGEX_OUTPUT_SYMBOL = "^(how much is )[a-z ]+(\\?)$";
+    private static final String REGEX_OUTPUT_CREDIT = "^(how many credits is )[a-z ]+(\\?)$";
     private SymbolRepository symbolRepository;
     private CreditRepository creditRepository;
     private SymbolWriter symbolWriter;
@@ -36,10 +40,10 @@ public class InputParser {
     private void parseWriterLine(String input) {
         Writer writer;
 
-        if (input.startsWith("how much")) {
+        if (input.matches(REGEX_OUTPUT_SYMBOL)) {
             writer = symbolWriter;
         }
-        else if (input.startsWith("how many")) {
+        else if (input.matches(REGEX_OUTPUT_CREDIT)) {
             writer = creditWriter;
         }
         else {
@@ -52,11 +56,11 @@ public class InputParser {
     private void parseRepositoryLine(String input) {
         Repository repository;
 
-        if (input.endsWith("credits")) {
-            repository = creditRepository;
-        }
-        else if (input.contains(" is ")) {
+        if (input.matches(REGEX_SET_SYMBOL)) {
             repository = symbolRepository;
+        }
+        else if (input.matches(REGEX_SET_CREDIT)) {
+            repository = creditRepository;
         }
         else {
             unknownWriter.process(input);
